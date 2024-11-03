b4280474b9fb1ddab1589f4394b01412d9702e55
Nguyen Son <truongson9a2nd@gmail.com>
2024-11-03 23:23:21 +0700

    Cap nhat

--- src/main/java/com/example/booker/entity/DonHangChiTiet.java
+++ src/main/java/com/example/booker/entity/DonHangChiTiet.java
@@ -39,11 +39,11 @@ public class DonHangChiTiet {
     @JoinColumn(name = "ma_trang_thai", insertable = false, updatable = false)
     TrangThaiDonHang trang_thai;
 
-    public DonHangChiTiet(String ten_san_pham, int so_luong, float thanh_tien) {
-        this.san_pham.ten_san_pham = ten_san_pham;
-        this.so_luong = so_luong;
-        this.thanh_tien = thanh_tien;
-    }
+//    public DonHangChiTiet(String ten_san_pham, int so_luong, float thanh_tien) {
+//        this.san_pham.ten_san_pham = ten_san_pham;
+//        this.so_luong = so_luong;
+//        this.thanh_tien = thanh_tien;
+//    }
     @ManyToOne
     @JoinColumn(name = "ma_san_pham", insertable=false, updatable=false)
     SanPham sanPham;
