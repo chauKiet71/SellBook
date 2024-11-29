d1268567af994c1f21cd5407c9cd0aa5fd6aa277
Nguyen Son <truongson9a2nd@gmail.com>
2024-11-29 14:12:33 +0700

    Add VNPAY

--- src/main/java/com/example/booker/dao/BaoCaoCuaHangDao.java
+++ src/main/java/com/example/booker/dao/BaoCaoCuaHangDao.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface  BaoCaoCuaHangDao extends JpaRepository<BaoCaoCuaHang, Integer> {
     //     Phương thức get list bao cao theo id_nguoi bị báo cáo
-    @Query("SELECT b FROM BaoCaoCuaHang b WHERE b.tai_khoan_bi_bao_cao.id_tai_khoan = :idtaikhoan AND b.trang_thai_bao_cao NOT IN (1, 4)")
+    @Query("SELECT b FROM BaoCaoCuaHang b WHERE b.tai_khoan_bi_bao_cao.id_tai_khoan = :idtaikhoan AND b.trang_thai_bao_cao.id_trang_thai_bao_cao NOT IN (1, 4)")
     List<BaoCaoCuaHang> findThongbaochotaikhoan(int idtaikhoan);
 
 //    lấy báo cáo đang xử lý và ko hợp leej - SELLER
